3e4149ca3854756da4534c7a94b190c98b4fa255
sabari471 <sabari.psmail@gmail.com>
2025-06-19 15:56:04 +0530

    added js

---
+++ index.js
@@ -0,0 +1,41 @@
+let a = 10;
+let b = 10.9;
+let c = "sabari"
+let d = true;
+let e = 123456788;
+console.log(e);
+ a = '+';
+ b = 1;
+ let j=1;
+ let f = a+b;
+ let g = a-b;
+ let h = a*b;
+ let i = a/b;
+ let l = a**b;
+ console.log(f && g);
+ console.log(f || g);
+ console.log(a == b);
+ console.log(a != b);
+ console.log(a,b);
+ console.log((a&&b),(b&&j));
+if (a == b){
+    console.log("a is equal to b");
+}
+if(b == j)
+{
+    console.log("b is = j");
+}
+else {
+    console.log("not equal");
+}
+
+switch(a){
+    case '+': console.log("add");
+    break;
+    case '-':console.log("sub");
+    default: comsole.log("error");
+}
+for (let i = 100; i >= 0; i--) {
+    console.log("Count: "  +i);
+}
+
